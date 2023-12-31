8f94e572b8f039abce4b8fcf56c1831a326bf50f
BeshoiBotros <beshoibotros111@gmail.com>
2023-12-31 13:56:01 +0200

    .

--- users/serializers.py
+++ users/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+# from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework import serializers
 from . import models
 from django.contrib.auth import get_user_model
