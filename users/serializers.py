81e9f650b511d1c7034afb2125c6b1869a86d07d
BeshoiBotros <beshoibotros111@gmail.com>
2024-01-24 14:03:07 +0200

    .

--- users/serializers.py
+++ users/serializers.py
@@ -14,9 +14,18 @@ class CustomUserSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(required=True)
     first_name = serializers.CharField(required=True)
     last_name = serializers.CharField(required=True)
-
+    is_client = serializers.BooleanField(read_only=True)
+    is_lawyer = serializers.BooleanField(read_only=True)
+    
     class Meta:
         model = models.CustomUser  
         fields = ['id', 'username', 'password', 'first_name', 'last_name', 'email', 'is_client', 'is_lawyer']
 
-
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        email = data.get('email')  # Using get to avoid KeyError
+        
+        if email and emailAlreadyExist(email):
+            raise serializers.ValidationError({'error': 'Email already exists'})
+        
+        return data
