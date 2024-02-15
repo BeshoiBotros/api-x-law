397f8a68f2fb316653f3f0e188b96a95966faba5
BeshoiBotros <beshoibotros111@gmail.com>
2024-02-15 18:07:17 +0200

    .

--- organizations/views.py
+++ organizations/views.py
@@ -6,6 +6,7 @@ from rest_framework.permissions import IsAuthenticated
 from XLaw import shortcuts
 from rest_framework.response import Response
 from . import filters
+from rest_framework import status
 
 class OrganizationView(APIView):
     
@@ -16,27 +17,56 @@ class OrganizationView(APIView):
         if pk:
             instance = shortcuts.object_is_exist(pk, models.Organization, 'organization not found')
             serializer = serializers.OrganizationSerializer(instance)
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         
         queryset = models.Organization.objects.filter(subscribe_contract__is_active=True)
         filter = filters.OrganizationFilter(request.GET, queryset=queryset)
         if filter.is_valid():
             queryset = filter.qs
         serializer = serializers.OrganizationSerializer(queryset, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request):
         can_add = shortcuts.check_permission('add_organization',request)
         if can_add:
-            pass
+            serializer = serializers.OrganizationSerializer(request.data)
+            if serializer.is_valid():
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         else:
-            return Response({'message' : 'You can not perform this action'})
+            return Response({'message' : 'You can not perform this action'}, status=status.HTTP_400_BAD_REQUEST)
 
     def patch(self, request, pk):
-        pass
+        can_add = shortcuts.check_permission('change_organization',request)
+        instance = shortcuts.object_is_exist(pk, models.Organization, "organizations not found")
+        if can_add:
+            serializer = serializers.OrganizationSerializer(instance, data=request.data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        if request.user.pk == instance.user.pk:
+            serializer = serializers.OrganizationSerializer(instance, data=request.data, partial=True)
+            if serializer.is_valid():
+                serializer.validated_data.pop('subscribe_contract', None)
+                serializer.validated_data.pop('user', None)
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response({'message' : 'You can not perform this action'})
 
     def delete(self, request, pk):
-        pass
+        can_add = shortcuts.check_permission('delete_organization',request)
+        instance = shortcuts.object_is_exist(pk, models.Organization, "organizations not found")
+        if can_add:
+            instance.delete()
+            return Response({'message':'Organization has been deleted successfully.'}, status=status.HTTP_200_OK)
+        else:
+            return Response({'message' : 'You can not perform this action'}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class OrganizationStaffView(APIView):
